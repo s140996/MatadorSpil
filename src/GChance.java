e70aba6d04cf25266aae5826b3eed238c58def72
Anders <s140996@student.dtu.dk>
2015-04-07 13:28:29 +0200

    GChance tilføjet

---
+++ src/GChance.java
@@ -0,0 +1,18 @@
+
+public class GChance extends GField {
+
+	int penis;
+
+	@Override
+	public void landOnField() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeOwner() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
