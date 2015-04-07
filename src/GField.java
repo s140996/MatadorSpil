7700c71eefceaba90805d122aa54b63076688eaa
madsmortensen <kjaermortensen@hotmail.com>
2015-04-07 10:56:57 +0200

    Tilføjet GField og GTax

---
+++ src/GField.java
@@ -0,0 +1,51 @@
+public abstract class GField {
+	
+	private String Name;
+	private int ID;
+	private String type;
+	
+	public String setName()
+	{
+		return Name;
+		
+	}
+	
+	public String getName()
+	{
+		return Name;
+		
+	}
+	
+	public int setID()
+	{
+		return ID;
+	}
+	
+	public int getID()
+	{
+		return ID;
+	}
+	
+	public String setType()
+	{
+		return type;
+	}
+	
+	public String getType()
+	{
+		return type;
+	}
+	
+	public abstract void landOnField()
+	{
+		
+	}
+	
+	public abstract void removeOwner()
+	{
+		
+	}
+	
+	
+
+}
---
+++ src/GTax.java
@@ -0,0 +1,30 @@
+public class GTax extends GField {
+
+	private int basetax;
+	private double percentageTax;
+	
+	public int getBasetax()
+	{
+		return basetax;
+	}
+	
+	public int getpercentageTax(int playerWorth);
+	{
+		
+	}
+
+	@Override
+	public void landOnField() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeOwner() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
