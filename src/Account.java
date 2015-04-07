17bd0423af4360f951feaf02bc6bae09201e5ea5
ChristofferSvendsen <s145089@student.dtu.dk>
2015-04-07 10:15:57 +0200

    Account

--- src/Account.java
+++ src/Account.java
@@ -3,6 +3,11 @@ public class Account {
 	
 	private int balance;
 	
+	public Account(int balance)
+	{
+		this.balance = balance;
+	}
+
 	public void setBalance(int newCash)
 	{
 		this.balance = this.balance + newCash;
