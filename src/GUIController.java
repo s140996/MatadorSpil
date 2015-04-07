9cc1605783101b475d051074fa5bcb17c1b32c02
Anders <s140996@student.dtu.dk>
2015-04-07 16:37:54 +0200

    Metoder tilføjet til GUIController

--- src/GUIController.java
+++ src/GUIController.java
@@ -1,6 +1,44 @@
+import java.awt.Color;
+
+import desktop_codebehind.Car;
+import desktop_resources.GUI;
 
 public class GUIController {
 
+	private Color[] color = {Color.RED, Color.blue, Color.green, Color.yellow, Color.black, Color.white};
+	private int i = 0;
+	 
+	 
+			// *** Add Player ***
+	public void addPlayer(String name)
+	{
+		Car car = new Car.Builder()
+        .primaryColor(color[i])
+        .typeRacecar()
+        .build();
+        i++;
+		
+		GUI.addPlayer(name, 30000, car);
+		GUI.setCar(1, name);
+	}
 	
+			// *** Create Field ***
+	public void createField(GField fields[])
+	{
+		GUI.create(fields[]);
+	}
+	
+			// *** Move Car ***
+	public void moveCar(int position, String name, int lastRoll)
+	{
+		GUI.removeCar(position, name);
+		GUI.setCar(position + lastRoll, name);
+	}
+	
+			// *** Set Balance ***
+	public void setGUIBalance(int newBalance, String name)
+	{
+		GUI.setBalance(name, newBalance);
+	}
 	
 }
--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -1,9 +1,26 @@
+import java.awt.Color;
+
+import desktop_codebehind.Car;
 import desktop_resources.GUI;
 
 
 public class GameLauncher {
 
 	public static void main(String[] args) {
+		
+		 Car car = new Car.Builder()
+         .typeRacecar()
+         .primaryColor(Color.BLUE)
+         .secondaryColor(Color.RED)
+         .patternDiagonalDualColor()
+         .build();
+		
+		GUI.addPlayer("Peter", 1000);
+		GUI.addPlayer("HEJ", 1000);
+		GUI.addPlayer("ter", 1000);
+		GUI.addPlayer("eter", 1000);
+		GUI.addPlayer("Pete", 1000);
+		GUI.addPlayer("diller", 1000, car);
 		
 		GUI.setDice(5, 4);
 
