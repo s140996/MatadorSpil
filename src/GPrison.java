4958cf2b658e7b28abc3e4a55611473b107c3f3b
Anders <s140996@student.dtu.dk>
2015-04-07 15:46:36 +0200

    GPrison extends GField

--- src/GPrison.java
+++ src/GPrison.java
@@ -1,4 +1,15 @@
+public class GPrison extends GField {
 
-public class GPrison {
+	@Override
+	public void landOnField(Player player, GUIController GGUI) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeOwner(Player player, int fieldnumber, GUIController GGUI) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
