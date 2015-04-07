2196ee116644c8b4fc1232f09adcf1723f0c9580
ChristofferSvendsen <s145089@student.dtu.dk>
2015-04-07 09:49:21 +0200

    Player class opdateret

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,24 @@
 public class Player {
 	
 	private String name;
-	
 	private int worth;
 	private int position;
 	
+	private Account acc = new Account();
+	
+	private void setPosition()
+	{
+		
+	}
+	
+	private void getPosition()
+	{
+		
+	}
+	
+	private void changePosition(int diceRoll)
+	{
+		
+	}
 
 }
