1b1a8da934743642e5b9f0c48c6da4280f3efdc6
Roos <Roos94@icloud.com>
2015-04-07 14:33:30 +0200

    Cup ændring

--- src/Cup.java
+++ src/Cup.java
@@ -1,38 +1,49 @@
 
 public class Cup {
 
+	private int dieOne;
+	private int dieTwo;
+	private int lastRoll;
+	private int doubleThrows;
+	
 	Die die = new Die();
 	
-	int doubleThrows;
-	
-	int dieOne = die.rollDie();
-	int dieTwo = die.rollDie();
-	
-	int lastRoll = dieOne + dieTwo;
+	public void roll()
+	{
+		this.dieOne = this.die.rollDie();
+		this.dieTwo = this.die.rollDie();
+		
+		this.lastRoll = this.dieOne + this.dieTwo;
+		
+		if (this.dieOne == this.dieTwo)
+		{
+			this.doubleThrows++;
+		}
+	}
 	
 	public int getLastRoll()
 	{
-		
+		return this.lastRoll;
 	}
 
-	public void resetDoubleThrows
+	public void resetDoubleThrows()
 	{
-		
+		this.doubleThrows = 0;
 	}
 	
 	public int getDoubleThrows()
 	{
-		
+		return this.doubleThrows;
 	}
 	
-	public int getDieOne
+	public int getDieOne()
 	{
-		
+		return this.dieOne;
 	}
 	
-	public int getDieTwo
+	public int getDieTwo()
 	{
-		
+		return this.dieTwo;
 	}
 	
 }
