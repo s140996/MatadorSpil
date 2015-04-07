7e9012e4d8d20da353f99b2b411a6314c29bd714
Anders <s140996@student.dtu.dk>
2015-04-07 13:11:54 +0200

    GUIController og Territory tilføjet

---
+++ src/GTerritory.java
@@ -0,0 +1,25 @@
+
+public class GTerritory extends GOwnable {
+
+	int rent;
+	int house;
+	int hotel;
+	
+	
+	@Override
+	public int getRent() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void landOnField() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void removeOwner() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
