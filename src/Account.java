f096d3b98a640356a9ad18c91d6330670c0c96e8
ChristofferSvendsen <s145089@student.dtu.dk>
2015-04-07 09:42:16 +0200

    Account oprettet

---
+++ src/Account.java
@@ -0,0 +1,25 @@
+
+public class Account {
+	
+	private int balance;
+	
+	public void setBalance(int newCash)
+	{
+		this.balance = this.balance + newCash;
+	}
+	
+	public int getBalance()
+	{
+		return balance;
+	}
+	
+	public int deposit()
+	{
+		
+	}
+
+	public int withdraw()
+	{
+		
+	}
+}
--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -3,9 +3,7 @@ public class GameLauncher {
 
 	public static void main(String[] args) {
 		
-		Dice dice = new Dice();
-		System.out.println (dice.rollDieOne());
-		System.out.println (dice.rollDieTwo());
+		
 
 	}
 
