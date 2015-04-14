ef7f30ae49c033164ba771f50d51d9628c8b37c5
SteenBartholdy <s145090@student.dtu.dk>
2015-04-14 16:53:29 +0200

    Pull pull

--- src/ChanceCard.java
+++ src/ChanceCard.java
@@ -4,6 +4,10 @@ public class ChanceCard {
 	private String text;
 	private int cash;
 	private int position;
+	private int houseTax;
+	private int hotelTax;
+	private boolean all;
+	private boolean rich;
 	
 	public ChanceCard (String text, int cash, int position)
 	{
@@ -12,6 +16,33 @@ public class ChanceCard {
 		this.position = position;
 	}
 	
+	// Matadorlegatet
+	public ChanceCard (String text, int cash, boolean rich)
+	{
+		this.text = text;
+		this.cash = cash;
+		this.rich = rich;
+	}
+	
+	//Ejendomsskat
+	public ChanceCard (int houseTax, int hotelTax, String text, boolean all)
+	{
+		this.text = text;
+		this.hotelTax = houseTax;
+		this.hotelTax = hotelTax;
+		this.all = all;
+	}
+	
+	//Fødselsdag
+	public ChanceCard (String text, int cash)
+	{
+		this.text = text;
+		this.cash = cash;
+		
+		
+	}
+	
+
 	public int getCash()
 	{
 		return this.cash;
