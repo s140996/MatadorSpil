c670972473a0c0b8852f21930fcc113be38ef8f0
Anders <s140996@student.dtu.dk>
2015-04-14 16:18:39 +0200

    player alive

--- src/Player.java
+++ src/Player.java
@@ -5,16 +5,18 @@ public class Player {
 	private int position;
 	private int prisonCard;
 	private boolean convict;
+	private boolean alive;
 	
 	public Account acc = new Account(30000);
 	
-	public Player(String name, int worth, int position, int prisonCard, boolean convict)
+	public Player(String name, int worth, int position, int prisonCard, boolean convict, boolean alive)
 	{
 		this.name = name;
 		this.worth = worth;
 		this.position = position;
 		this.prisonCard = prisonCard;
 		this.convict = false;
+		this.alive = true;
 	}
 	
 	public void setPosition(int newPosition)
@@ -73,6 +75,17 @@ public class Player {
 		return convict;
 	}
 
+	public void setAlive(boolean alive)
+	{
+		this.alive = alive;
+	}
+	
+	public boolean getAlive()
+	{
+		return alive;
+	}
+	
+	public void playerLost(booelan )
 }
 
 
