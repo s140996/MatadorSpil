a7efb7bc6d55123b3625cadf370393c3c5bc43ed
Anders <s140996@student.dtu.dk>
2015-04-21 15:38:22 +0200

    text

--- src/ChanceCardList.java
+++ src/ChanceCardList.java
@@ -5,19 +5,19 @@ public class ChanceCardList {
 	private int index;
 	
 	private ArrayList<ChanceCard> list = new ArrayList<ChanceCard>();
-	private ChanceCard c1 = new ChanceCard("Du modtager 1000 kr i aktieudbytte.", 1000, 0);
+	private ChanceCard c1 = new ChanceCard("Du modtager dit aktieudbytte 1000 kr.", 1000, 0);
 	private ChanceCard c2 = new ChanceCard("Ryk frem til start.", 0, 1);
 	private ChanceCard c3 = new ChanceCard("Gå i fængsel.", 0, 11);
 	private ChanceCard c4 = new ChanceCard("Gå i fængsel.", 0, 11);
-	private ChanceCard c5 = new ChanceCard("Told - for mange cigaretter.", -200, 0);
-	private ChanceCard c6 = new ChanceCard("Tandlægeregning.", -2000, 0);
-	private ChanceCard c7 = new ChanceCard("Tip 13.", 1000, 0);
-	private ChanceCard c8 = new ChanceCard("Præmieobligationer.", 1000, 0);
-	private ChanceCard c9 = new ChanceCard("Præmieobligationer.", 1000, 0);
+	private ChanceCard c5 = new ChanceCard("Du har haft for mange cigaretter med hjem. Betal 200 kr. i told-bøde", -200, 0);
+	private ChanceCard c6 = new ChanceCard("Du har modtaget din tandlægeregning. Betal 2000 kr.", -2000, 0);
+	private ChanceCard c7 = new ChanceCard("Du har haft 12 rigtige i tips 13. Modtag 1000 kr.", 1000, 0);
+	private ChanceCard c8 = new ChanceCard("Din restskat er gået ind. Modtag 1000 kr.", 1000, 0);
+	private ChanceCard c9 = new ChanceCard("Du har solgt din gamle velo. Modtag 1000 kr.", 1000, 0);
 	// Skal have funktion med, så det er alle der betaler til fødselaren
-	private ChanceCard c10 = new ChanceCard("Din fødselsdag. Modtag 200 fra hver medspiller", 200);
-	private ChanceCard c11 = new ChanceCard("Nyttehaven af avl.", 200, 0);
-	private ChanceCard c12 = new ChanceCard("Bilforsikring.", -1000, 0);
+	private ChanceCard c10 = new ChanceCard("Det er din fødselsdag. Modtag 200 kr. fra hver medspiller.", 200);
+	private ChanceCard c11 = new ChanceCard("Du har fundet en pung på gaden. Stjæl de 200 kr der ligger i den.", 200, 0);
+	private ChanceCard c12 = new ChanceCard("Du har modtaget din vægtafgift. Betal 1000 kr.", -1000, 0);
 	// Ejendomsskat: 800 pr. hus og 2300 pr. hotel (Y)
 	private ChanceCard c13 = new ChanceCard(800, 2300, "Ejendomsskatten er steget.", true);
 	private ChanceCard c14 = new ChanceCard("Ryk frem til Grønningen", 0, 25);
