89068581f3bb3874e8396b5f81d55742cedbd21e
Anders <s140996@student.dtu.dk>
2015-04-21 12:25:59 +0200

    brewery og fleet opdateret

--- src/GFleet.java
+++ src/GFleet.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 public class GFleet extends GOwnable {
 
 	private int rent;
-	
+
 	public GFleet (int id, String name, int price, int rent)
 	{
 		super.setID(id);
@@ -16,49 +16,49 @@ public class GFleet extends GOwnable {
 
 	@Override
 	public int getRent() {
-		
+
 		if (super.getOwner().getFleetsOwned() == 1)
 		{
 			rent = 500;
 		}
-		
+
 		else if (super.getOwner().getFleetsOwned() == 2)
 		{
 			rent = 1000;
 		}
-		
+
 		if (super.getOwner().getFleetsOwned() == 3)
 		{
 			rent = 2000;
 		}
-		
+
 		if (super.getOwner().getFleetsOwned() == 4)
 		{
 			rent = 4000;
 		}
-		
+
 		return rent;
 	}
 
 	@Override
 	public void landOnField(Player player, GUIController GGUI, ChanceCardList cc, int lastRoll) {
-		
+
 		if (super.isOwned() == false)
 		{
 			boolean reply = GGUI.boolButton("Vil du købe Færgen?", "Ja", "Nej");
-			
-				if(reply == true)
-				{
-					super.setOwner(player);
-					GGUI.setOwner(super.getID(), player.toString());
-					player.acc.deposit(-super.getPrice());
-					GGUI.setGUIBalance(player.acc.getBalance(), player.toString());
-					player.setFleetsOwned(player.getFleetsOwned()+1);
-				}
-				else if(reply == false)
-				{
-					GGUI.showMessage("Du valgte ikke at købe færgen");
-				}
+
+			if(reply == true)
+			{
+				super.setOwner(player);
+				GGUI.setOwner(super.getID(), player.toString());
+				player.acc.deposit(-super.getPrice());
+				GGUI.setGUIBalance(player.acc.getBalance(), player.toString());
+				player.setFleetsOwned(player.getFleetsOwned()+1);
+			}
+			else if(reply == false)
+			{
+				GGUI.showMessage("Du valgte ikke at købe færgen");
+			}
 		}
 
 		else if(super.isOwned() == true)
@@ -67,26 +67,34 @@ public class GFleet extends GOwnable {
 			{
 				GGUI.showMessage("Du ejer den selv!");
 			}
-		
+
 			else
 			{
-				player.acc.deposit(-getRent());
-				super.getOwner().acc.deposit(getRent());
-				
-				GGUI.setGUIBalance(player.acc.getBalance(), player.toString());
-				GGUI.setGUIBalance(super.getOwner().acc.getBalance(), super.getOwner().toString());
+				if (super.getOwner().getConvict() == true)
+				{
+
+				}
+
+				else 
+				{
+					player.acc.deposit(-getRent());
+					super.getOwner().acc.deposit(getRent());
+
+					GGUI.setGUIBalance(player.acc.getBalance(), player.toString());
+					GGUI.setGUIBalance(super.getOwner().acc.getBalance(), super.getOwner().toString());
+				}
 			}	
-			}
-		
 		}
+
+	}
 
 	@Override
 	public void removeOwner(Player player, int fieldnumber, GUIController GGUI) {
-		
+
 		super.setOwner(null);
 		player.setFleetsOwned(player.getFleetsOwned()-1);
 		GGUI.removeOwner(fieldnumber);
-		
+
 	}
-	
+
 }
