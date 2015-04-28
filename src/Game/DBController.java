5d4e9e0172f6d1c76d96e9b276dbc930c40bc9e3
SteenBartholdy <s145090@student.dtu.dk>
2015-04-28 10:41:07 +0200

    waah

--- src/Game/DBController.java
+++ src/Game/DBController.java
@@ -6,13 +6,12 @@ public class DBController {
 	public static void main(String[] args) throws Exception {
 		//load driveren
 		Class.forName("com.mysql.jdbc.Driver");
-		//opret forbindelse til databasen "test2"
+		//opret forbindelse til databasen "Matador"
 		Connection con= DriverManager.getConnection(
 				"jdbc:mysql://localhost:3306/Matador","root","");
-
 		//opret sætningsobjekt
 		Statement s= con.createStatement();
-
-
+		
+		
 	}
 }
