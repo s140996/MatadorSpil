e76463116c280273020e267b6c35373e8fc25a9d
madsmortensen <kjaermortensen@hotmail.com>
2015-04-28 10:45:46 +0200

    JUnit test player.Account

--- src/Test/AccountTest.java
+++ src/Test/AccountTest.java
@@ -3,12 +3,30 @@ package Test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import Player.Account;
+
 
 public class AccountTest {
 
+	Account Account = new Account(5000);
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testgetBalance() {
+		Account.getBalance();
+		assertEquals(Account.getBalance(),5000);
+	}
+	
+	@Test
+	public void testsetBalance() {
+		Account.setBalance(10000);
+		assertEquals(Account.getBalance(),10000);
+	}
+	
+	@Test
+	public void testdeposit() {
+		Account.deposit(5000);
+		assertEquals(Account.getBalance(),10000);
 	}
 
 }
