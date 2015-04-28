b968d59309bb56886632d67ce1faf628114a84f1
SteenBartholdy <s145090@student.dtu.dk>
2015-04-28 10:17:33 +0200

    Database startet

---
+++ src/DBController.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class DBController {
+
+	public static void main(String[] args) throws Exception {
+		//load driveren
+		Class.forName("com.mysql.jdbc.Driver");
+		//opret forbindelse til databasen "test2"
+		Connection con= DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/Matador","root","");
+
+		//opret sætningsobjekt
+		Statement s= con.createStatement();
+
+
+	}
+}
