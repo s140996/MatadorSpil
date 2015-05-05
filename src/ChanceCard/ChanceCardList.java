3092a2a159c4e200fe282d73c202d330baff3bc7
Anders <s140996@student.dtu.dk>
2015-05-05 15:23:46 +0200

    cc

--- src/ChanceCard/ChanceCardList.java
+++ src/ChanceCard/ChanceCardList.java
@@ -6,19 +6,19 @@ public class ChanceCardList {
 	private int index;
 
 	private ArrayList<ChanceCard> list = new ArrayList<ChanceCard>();
-	private ChanceCard c1 = new ChanceCard("CHANCEKORT: Du modtager dit aktieudbytte på 1000,-.", 1000, 0);
+	private ChanceCard c1 = new ChanceCard("CHANCEKORT: Du har investeret i Novo Nordisk aktier, og det er gået godt. Modtag 1000,-.", 1000, 0);
 	private ChanceCard c2 = new ChanceCard("CHANCEKORT: Ryk frem til start.", 0, 1);
-	private ChanceCard c3 = new ChanceCard("CHANCEKORT: Gå i fængsel.", 0, 11);
-	private ChanceCard c4 = new ChanceCard("CHANCEKORT: Gå i fængsel.", 0, 11);
-	private ChanceCard c5 = new ChanceCard("CHANCEKORT: Du har haft for mange cigaretter med hjem. Betal 200,- i told-bøde.", -200, 0);
-	private ChanceCard c6 = new ChanceCard("CHANCEKORT: Du har modtaget din tandlægeregning. Betal 2000,-.", -2000, 0);
-	private ChanceCard c7 = new ChanceCard("CHANCEKORT: Du har haft 12 rigtige i tips 13. Modtag 1000,-.", 1000, 0);
-	private ChanceCard c8 = new ChanceCard("CHANCEKORT: Din restskat er gået ind. Modtag 1000,-.", 1000, 0);
+	private ChanceCard c3 = new ChanceCard("CHANCEKORT: Bang! Du har kørt en gammel dame ned!", 0, 11);
+	private ChanceCard c4 = new ChanceCard("CHANCEKORT: Din netstrømpe var hullet, du er afsløret. Slikland har anmeldt dig!", 0, 11);
+	private ChanceCard c5 = new ChanceCard("CHANCEKORT: Skoene du har købt på nettet fra USA, er blevet fanget i tolden. Betal en bøde på 200,-", -200, 0);
+	private ChanceCard c6 = new ChanceCard("CHANCEKORT: Betal venligst dit hustrubidrag på 2000,-.", -2000, 0);
+	private ChanceCard c7 = new ChanceCard("CHANCEKORT: Du er blevet national mester i Hearthstone Modtag 1000,-.", 1000, 0);
+	private ChanceCard c8 = new ChanceCard("CHANCEKORT: Du har solgt din iPhone. Modtag 1000,-.", 1000, 0);
 	private ChanceCard c9 = new ChanceCard("CHANCEKORT: Du har solgt din gamle Velo Solex 3800S. Modtag 1000,-.", 1000, 0);
 	// Skal have funktion med, så det er alle der betaler til fødselaren
 	private ChanceCard c10 = new ChanceCard("CHANCEKORT: Du er blevet konfirmeret, og har fået 1000,- af din bedstemor.", 1000);
 	private ChanceCard c11 = new ChanceCard("CHANCEKORT: Du har fundet en pung på gaden. Stjæl de 200,- der ligger i den.", 200, 0);
-	private ChanceCard c12 = new ChanceCard("CHANCEKORT: Du har modtaget din vægtafgift. Betal 1000,-.", -1000, 0);
+	private ChanceCard c12 = new ChanceCard("CHANCEKORT: Du har modtaget anmodning på Mobilepay, efter hård tur i byen. Betal 1000,-.", -1000, 0);
 	// Ejendomsskat: 800 pr. hus og 2300 pr. hotel (Y)
 	private ChanceCard c13 = new ChanceCard(800, 2300, "CHANCEKORT: Ejendomsskatten er steget.", true);
 	private ChanceCard c14 = new ChanceCard("CHANCEKORT: Ryk frem til Grønningen", 0, 25);
@@ -29,22 +29,22 @@ public class ChanceCardList {
 	private ChanceCard c17 = new ChanceCard("CHANCEKORT: Tag med LB færgen", 0, 6);
 	// Benådes for fængsel må ikke rettes, uden også at rette i GChance
 	private ChanceCard c18 = new ChanceCard("CHANCEKORT: Du benådes for fængsel", 0, 0);
-	private ChanceCard c19 = new ChanceCard("CHANCEKORT: Lønforhøjelse på 1000,-.", 1000, 0);
-	private ChanceCard c20 = new ChanceCard("CHANCEKORT: Ryk frem til Frederiksberh Allé.", 0, 12);
-	private ChanceCard c21 = new ChanceCard("CHANCEKORT: Vundet i klasselotteriet. Modtag 500,-.", 500, 0);
+	private ChanceCard c19 = new ChanceCard("CHANCEKORT: Du har solgt dine gamle antikke Pokémon spil på auktion. Modtag 1000,-.", 1000, 0);
+	private ChanceCard c20 = new ChanceCard("CHANCEKORT: Ryk frem til Frederiksberg Allé.", 0, 12);
+	private ChanceCard c21 = new ChanceCard("CHANCEKORT: Du har vundet en konkurrence i at spise flest hot-dogs på 8 minutter. Modtag 500,-.", 500, 0);
 	private ChanceCard c22 = new ChanceCard("CHANCEKORT: Tag ind til Rådhuspladsen", 0, 40);
 	private ChanceCard c23 = new ChanceCard("CHANCEKORT: Ryk tre felter tilbage.", 0, -3);
 	// 500 pr. hus og 2000 pr. hotel (Y)
-	private ChanceCard c24 = new ChanceCard("CHANCEKORT: Oliepriserne er steget.", true);
-	private ChanceCard c25 = new ChanceCard("CHANCEKORT: Reparation af vogn. Betal 3000,-.", -3000, 0);		
-	private ChanceCard c26 = new ChanceCard("CHANCEKORT: Reparation af vogn. Betal ,-.", -3000, 0);		
+	private ChanceCard c24 = new ChanceCard("CHANCEKORT: Oliepriserne er steget. Betal 500,- pr. hus og 2000,- pr. hotel.", true);
+	private ChanceCard c25 = new ChanceCard("CHANCEKORT: Du har fået din regning efter din tandblegning. Betal 3000,-.", -3000, 0);		
+	private ChanceCard c26 = new ChanceCard("CHANCEKORT: Suzuki Torben er kommet for at indrive sin gæld. Betal 3000,-", -3000, 0);		
 	// +40000, hvis formue ikke overstiger 15000 kr
 	private ChanceCard c27 = new ChanceCard("CHANCEKORT: Matadorlegatet.", 40000, false);
-	private ChanceCard c28 = new ChanceCard("CHANCEKORT: Eftergivet skat. Modtag 3000,-.", 3000, 0);
-	private ChanceCard c29 = new ChanceCard("CHANCEKORT: Aktieudbytte på 1000,-.", 1000, 0);
+	private ChanceCard c28 = new ChanceCard("CHANCEKORT: Du har været med i det årlige bingospil ned i sportshallen og vundet! Modtag en gavekurv, 3000,- og en helstegt pattegris.", 3000, 0);
+	private ChanceCard c29 = new ChanceCard("CHANCEKORT: Din gamle udlejer har efterreguleret din husleje. Modtag 1000,-.", 1000, 0);
 	private ChanceCard c30 = new ChanceCard("CHANCEKORT: Aktieudbytte på 1000,-.", 1000, 0);
-	private ChanceCard c31 = new ChanceCard("CHANCEKORT: Kørt frem for Fuld stop. Betal 1000,-.", -1000, 0);
-	private ChanceCard c32 = new ChanceCard("CHANCEKORT: Parkeringsbøde på 200,-.", -200, 0);
+	private ChanceCard c31 = new ChanceCard("CHANCEKORT: Du har smadret dit hotelværelse og skylder dem 1000,- for de ødelagte genstande.", -1000, 0);
+	private ChanceCard c32 = new ChanceCard("CHANCEKORT: Parkeringsbøde på 510,-.", -510, 0);
 
 	public ChanceCardList()
 	{
